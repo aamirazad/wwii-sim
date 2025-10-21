0279e9627f15ee8c83ef8ac59d40a32b1261a95c
Aamir Azad <aamirmazad@gmail.com>
2025-10-21 19:17:35 -0400

    feat(ci): biome lint action

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 import "./src/env.js";
 
 const nextConfig: NextConfig = {
-	allowedDevOrigins: ["https://3000.aamira.me"]
+	allowedDevOrigins: ["https://3000.aamira.me"],
 };
 
 export default nextConfig;
