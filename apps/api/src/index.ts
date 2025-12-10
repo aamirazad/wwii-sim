fdc8e358af21933bf49543122b12acf6b53061a8
Aamir Azad <aamirmazad@gmail.com>
2025-12-10 18:16:48 -0500

    feat: eden treaty connection to next

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,17 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3001);
+const app = new Elysia()
+	.get("/", () => "Hello Elysia")
+	.get("/ping", () => "Pong")
+	.ws("/ws", {
+		message(ws, message) {
+			ws.send(message);
+		},
+	})
+	.listen(3001);
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
+
+export type App = typeof app;
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-	return <div className="">Hello there</div>;
+import { elysia } from "@api/src/client";
+
+export default async function Home() {
+	const data = await elysia.get();
+
+	return <div className="">Hello there from next. {data.data}</div>;
 }
