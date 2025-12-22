5f001795e9ae9510e66321b50b2dd546ae3b19d2
Aamir Azad <aamirmazad@gmail.com>
2025-12-21 19:16:36 -0500

    feat(web): pretty landing page with pixel blast

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	allowedDevOrigins: ["3000.aamirazad.com"],
+	env: {
+		NEXT_PUBLIC_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA,
+	},
 };
 
 export default nextConfig;
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -19,12 +19,30 @@ export default function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const commitHash = process.env.NEXT_PUBLIC_COMMIT_SHA?.substring(0, 7);
+
 	return (
 		<html suppressHydrationWarning lang="en">
 			<body
-				className={`${geistMono.variable} ${nunitoSans.variable} dark antialiased`}
+				className={`${geistMono.variable} ${nunitoSans.variable} dark antialiased flex flex-col min-h-screen`}
 			>
-				{children}
+				<main className="grow">{children}</main>
+				<footer className="relative z-10 p-4 flex flex-col items-center">
+					<div className="opacity-30 text-sm">
+						{commitHash ? (
+							<p>
+								Build{" "}
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									href={`https://github.com/aamirazad/wwii-sim/tree/${process.env.NEXT_PUBLIC_COMMIT_SHA}`}
+								>
+									{commitHash}
+								</a>
+							</p>
+						) : null}
+					</div>
+				</footer>
 			</body>
 		</html>
 	);
