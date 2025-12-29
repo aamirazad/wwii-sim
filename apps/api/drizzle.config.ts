adfc88148c012999845fc1d00e5db527ddb10046
Aamir Azad <aamirmazad@gmail.com>
2025-12-29 18:41:13 -0500

    feat(api): sqlite database with user table
    Basic authentication with token auth and request verification

---
+++ apps/api/drizzle.config.ts
@@ -0,0 +1,12 @@
+/** biome-ignore-all lint/style/noNonNullAssertion: required env */
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+	out: "./drizzle",
+	schema: "./src/db/schema.ts",
+	dialect: "sqlite",
+	dbCredentials: {
+		url: process.env.DB_FILE_NAME!,
+	},
+});
--- apps/api/src/client.ts
+++ apps/api/src/client.ts
@@ -1,12 +1,5 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "./index";
 
-// If you are not using Next.js v15^, you may want to set revalidate value to 0 due to default caching mechanics.
-
-// export const elysia = treaty<TElysiaApp>('localhost:3000',{
-//     fetch: {
-//       next:{revalidate:0}
-//     },
-//   })
 const url = "localhost:3001";
 export const elysia = treaty<App>(url);
