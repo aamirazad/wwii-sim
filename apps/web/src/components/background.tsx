fcb2dfb17a864c548898e7a65cacbd8c887e0b61
Aamir Azad <aamirmazad@gmail.com>
2026-01-12 13:09:24 -0500

    feat(web): non animated background

--- apps/web/src/components/background.tsx
+++ apps/web/src/components/background.tsx
@@ -1,10 +1,17 @@
+"use client";
+import { usePathname } from "next/navigation";
 import PixelBlast from "@/components/pixel-blast";
 
-export default async function Background({
+export default function Background({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const pathname = usePathname();
+
+	const isGamePage =
+		pathname in ["/game/resources", "/game/battles", "/game/info"];
+
 	return (
 		<>
 			<div className="fixed inset-0 opacity-40 fade-in">
@@ -15,11 +22,11 @@ export default async function Background({
 					patternScale={2}
 					patternDensity={1}
 					pixelSizeJitter={1}
-					enableRipples
+					enableRipples={!isGamePage}
 					rippleSpeed={0.9}
 					rippleThickness={0.1}
 					rippleIntensityScale={1}
-					speed={0.1}
+					speed={isGamePage ? 0 : 0.1}
 					edgeFade={0.05}
 					transparent
 				/>
