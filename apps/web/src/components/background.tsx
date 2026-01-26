88d35c8a59837c2bf510a62858395836a7f08456
Aamir Azad <aamirmazad@gmail.com>
2026-01-25 19:50:04 -0500

    revert: slow website loading due to client side loaded background
    This reverts commit fcb2dfb17a864c548898e7a65cacbd8c887e0b61.

--- apps/web/src/components/background.tsx
+++ apps/web/src/components/background.tsx
@@ -1,17 +1,10 @@
-"use client";
-import { usePathname } from "next/navigation";
 import PixelBlast from "@/components/pixel-blast";
 
-export default function Background({
+export default async function Background({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	const pathname = usePathname();
-
-	const isGamePage =
-		pathname in ["/game/resources", "/game/battles", "/game/info"];
-
 	return (
 		<>
 			<div className="fixed inset-0 opacity-40 fade-in">
@@ -22,11 +15,11 @@ export default function Background({
 					patternScale={2}
 					patternDensity={1}
 					pixelSizeJitter={1}
-					enableRipples={!isGamePage}
+					enableRipples
 					rippleSpeed={0.9}
 					rippleThickness={0.1}
 					rippleIntensityScale={1}
-					speed={isGamePage ? 0 : 0.1}
+					speed={0.1}
 					edgeFade={0.05}
 					transparent
 				/>
