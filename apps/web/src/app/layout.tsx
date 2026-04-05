4f0efe895751d609d64b76a7637a383388c3f4d4
Aamir Azad <aamirmazad@gmail.com>
2026-04-05 19:45:13 -0400

    revert: remove analytics script
    This reverts commit 113bf787abecc6c2d85ce1061e6edfabdff23ca9.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import { Providers } from "./providers";
 
 const nunitoSans = Nunito_Sans({ variable: "--font-nunito" });
@@ -25,6 +26,12 @@ export default function RootLayout({
 			<body
 				className={`${geistMono.variable} ${nunitoSans.variable} dark antialiased flex flex-col min-h-screen`}
 			>
+				<Script
+					src="https://analytics.aamirazad.com/api/script.js"
+					data-site-id="e7360f70ea78"
+					strategy="lazyOnload"
+					defer
+				></Script>
 				<Providers>
 					<main className="grow flex flex-col">{children}</main>
 				</Providers>
